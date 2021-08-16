fed6fa380d0d3457fc8a5cc97c82fa55e7952218
Al-amintpi <alamincmt7418@gmail.com>
2021-08-17 02:55:47 +0600

    dashboard frontend

--- admin/templates/dashboard/dashboard.php
+++ admin/templates/dashboard/dashboard.php
@@ -1,4 +1,55 @@
  <div class="wrap">
-    <h1 class="wp-heading-inline"><?php _e( 'Dashboard Page', 'lmfwppt' ); ?></h1>
+    <h1 class="wp-heading-inline"><?php _e( 'Overview', 'lmfwppt' ); ?></h1>
+</div>
+ 
+
+<div class="lmwppt-main-section">
+
+   <div class="lmwppt-inner">
+      <div class="lmwppt-content">
+
+         <div class="lmwppt-title">
+            <h3 class="wp-heading-inline"><?php _e( 'Total Plugin', 'lmfwppt' ); ?></h3>
+         </div>
+         <div class="lmwppt-icon">
+           <span class="dashicons dashicons-admin-plugins lmwppt-icon-2"></span>
+         </div>
+
+      </div>
+      <div class="lmwppt-counter">
+         <h1><?php echo LMFWPPT_ProductsListTable::product_count('plugin'); ?></h1>
+      </div>
+   </div>
+
+   <div class="lmwppt-inner">
+      <div class="lmwppt-content">
+
+         <div class="lmwppt-title">
+            <h3 class="wp-heading-inline"><?php _e( 'Total Theme', 'lmfwppt' ); ?></h3>
+         </div>
+         <div class="lmwppt-icon">
+            <span class="dashicons dashicons-admin-appearance lmwppt-icon-3"></span>
+         </div>
+
+      </div>
+      <div class="lmwppt-counter">
+         <h1><?php echo LMFWPPT_ProductsListTable::product_count('theme'); ?></h1>
+      </div>
+   </div>
+   
+   <div class="lmwppt-inner">
+      <div class="lmwppt-content">
+         <div class="lmwppt-title">
+            <h3 class="wp-heading-inline"><?php _e( 'Total License', 'lmfwppt' ); ?></h3>
+         </div>
+         <div class="lmwppt-icon">
+            <span class="dashicons dashicons-tickets-alt lmwppt-icon-1"></span>
+         </div>
+      </div>
+      <div class="lmwppt-counter">
+         <h1><?php echo LMFWPPT_LicenseListTable::license_count(); ?></h1>
+      </div>
+   </div>
+
 </div>
 
