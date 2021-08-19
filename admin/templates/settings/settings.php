eac66a19adac091a5561e92904eb9a4b7263a4ef
Al-amintpi <alamincmt7418@gmail.com>
2021-08-19 15:44:51 +0600

    setting page field add

--- admin/templates/settings/settings.php
+++ admin/templates/settings/settings.php
@@ -13,13 +13,25 @@
          <table class="form-table" role="presentation">
             <tbody>
                <tr>
-                  <th scope="row"><label for="license_code_prefix">License Code Prefix</label></th>
+                  <th scope="row"><label for="license_code_prefix"><?php esc_html_e('License Code Prefix', 'lmfwppt') ?></label></th>
                   <td><input type="text" name="lmfwppt_settings[license_code_prefix]" id="license_code_prefix" class="regular-text" placeholder="<?php esc_attr_e( 'License Code Prefix', 'lmfwppt' ); ?>" value="<?php echo $code_prefix  ?>"></td>
                </tr>
                <tr>
-                  <th scope="row"><label for="license_code_character_limit">License Code Character Limit</label></th>
+                  <th scope="row"><label for="license_code_character_limit"><?php esc_html_e('License Code Character Limit', 'lmfwppt') ?></label></th>
                   <td><input type="number" name="lmfwppt_settings[license_code_character_limit]" id="license_code_character_limit" class="regular-text" placeholder="<?php esc_attr_e( 'License Code Character Limit', 'lmfwppt' ); ?>" value="<?php echo $character_limit; ?>"></td>
                </tr>
+               <tr>
+                  <th scope="row"><?php esc_html_e( 'Hide License Info from WooCommerce', 'lmfwppt' ); ?></th>
+                   <td>
+                       <fieldset><label>
+                           <input name="lmfwppt_settings[hide_wclm_info_from_cart]" type="checkbox" id="hide_cart_checkout"><?php esc_html_e( 'Hide from Cart & Checkout', 'lmfwppt' ); ?></label>
+                       </fieldset>
+                       <fieldset><label>
+                           <input name="lmfwppt_settings[hide_wclm_info_from_ordermeta]" type="checkbox" id="hide_order_email"><?php esc_html_e( 'Hide from Order Email', 'lmfwppt' ); ?></label>
+                       </fieldset>
+                   </td>
+               </tr>
+
             </tbody>
          </table>
          <input type="hidden" name="lmaction" value="setting_add_form">
