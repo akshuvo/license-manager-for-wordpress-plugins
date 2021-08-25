c00362faf084d25aa93a16ae4c1fd0be84789638
Al-amintpi <alamincmt7418@gmail.com>
2021-08-26 04:19:03 +0600

    popup convert table

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,8 +2,17 @@
 	"use strict";
 	$(document).ready(function(){
 		
-
+		$(document).on('click', '.show_manage_activations_details a', function(e){
+            e.preventDefault();
+            console.log("hi");
+            $(this).closest('td').find('.manage-activations').toggleClass("active");
+        });
+        $(document).on('click', '.manage-activations a', function(e){
+            e.preventDefault();
+            $(this).closest('td').find('.manage-activations').removeClass("active");
+        });
 
 
 	});
+
 })(jQuery);
